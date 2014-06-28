b465f65b4e46bece9e89844dd4c5496dee148e3f
Max Hohenegger <github.com@hohenegger.eu>
2014-06-28 18:40:38 +0200

    removed warnings; added css

--- src/main/java/eu/hohenegger/clippingsparser/gui/Starter.java
+++ src/main/java/eu/hohenegger/clippingsparser/gui/Starter.java
@@ -1,37 +1,32 @@
 package eu.hohenegger.clippingsparser.gui;
 
+
+
 import javafx.application.Application;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.MenuItemBuilder;
-import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.SplitPane;
-import javafx.scene.control.SplitPaneBuilder;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.ToolBar;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.layout.VBoxBuilder;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -46,13 +41,16 @@ import eu.hohenegger.clippingsparser.model.Clipping;
 @SuppressWarnings("restriction")
 public class Starter extends Application {
 
+	private static double initialY = 450;
+	private static double initialX = 800;
 	private static final int CONTENT_HEIGHT = 60;
-	TableView<Clipping> table = new TableView<Clipping>();
+	private TableView<Clipping> table = new TableView<Clipping>();
 	private Text contentBox;
 
-	private final class OpenHandler implements EventHandler<ActionEvent> {
+	
+	private final class OpenHandler implements EventHandler<MouseEvent> {
 		@Override
-		public void handle(ActionEvent event) {
+		public void handle(MouseEvent event) {
 			FileChooser fileChooser = new FileChooser();
 
 			FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
@@ -75,6 +73,7 @@ public class Starter extends Application {
 			}
 
 		}
+
 	}
 
 	public static void main(String[] args) throws IOException,
@@ -86,10 +85,43 @@ public class Starter extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+//		stage.initStyle(StageStyle.UNDECORATED);
+		
 		stage.setTitle("MyClippingsViewer");
 		final Group rootGroup = new Group();
-		final Scene scene = new Scene(rootGroup, 800, 450, Color.WHITE);
-		final MenuBar menuBar = buildMenuBarWithMenus(stage.widthProperty());
+		Scene scene = new Scene(rootGroup, initialX, initialY, Color.WHITE);
+		
+
+		scene.getStylesheets().add(this.getClass().
+		getResource("/css/base.css").toExternalForm());
+
+
+		Button openButton = new Button("Open");
+		openButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new OpenHandler());
+		
+		Button exitButton = new Button("Exit");
+		exitButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> System.exit(0));
+		
+		ToolBar toolBar = new ToolBar(openButton, exitButton);
+//		toolBar.setOnMousePressed(new EventHandler<MouseEvent>() {
+//	        @Override
+//	        public void handle(MouseEvent me) {
+//	            if (me.getButton() != MouseButton.MIDDLE) {
+//	                initialX = me.getSceneX();
+//	                initialY = me.getSceneY();
+//	            }
+//	        }
+//	    });
+//
+//		toolBar.setOnMouseDragged(new EventHandler<MouseEvent>() {
+//	        @Override
+//	        public void handle(MouseEvent me) {
+//	            if (me.getButton() != MouseButton.MIDDLE) {
+//	            	toolBar.getScene().getWindow().setX(me.getScreenX() - initialX);
+//	            	toolBar.getScene().getWindow().setY(me.getScreenY() - initialY);
+//	            }
+//	        }
+//	    });
 
 		final VBox vbox = new VBox();
 		vbox.setSpacing(0);
@@ -100,11 +132,10 @@ public class Starter extends Application {
 		StackPane lowerPane = new StackPane();
 		SplitPane.setResizableWithParent(upperPane, true);
 		SplitPane.setResizableWithParent(lowerPane, false);
-		final SplitPane splitPane = SplitPaneBuilder.create()
-				.items(upperPane, lowerPane)
-				// Environment A
-				.dividerPositions(new double[] { 0.7, 0.3 })
-				.orientation(Orientation.VERTICAL).build();
+		SplitPane splitPane = new SplitPane();
+		splitPane.getItems().addAll(upperPane, lowerPane);
+		splitPane.setDividerPositions(new double[] { 0.7, 0.3 });
+		splitPane.setOrientation(Orientation.VERTICAL);
 
 		upperPane.getChildren().add(buildTable(stage.widthProperty()));
 		contentBox = new Text();
@@ -113,29 +144,34 @@ public class Starter extends Application {
 		
 
 
-		stage.heightProperty().addListener((ChangeListener<Number>) (arg0, oldValue, newValue) -> splitPane.setPrefHeight(newValue.doubleValue() - CONTENT_HEIGHT));
+		stage.heightProperty()
+				.addListener((observableValue, oldValue, newValue) -> splitPane
+								.setPrefHeight(newValue.doubleValue()
+										- CONTENT_HEIGHT));
 		
-		
+		stage.widthProperty()
+				.addListener((observableValue, oldValue, newValue) -> contentBox
+								.setWrappingWidth(newValue.doubleValue() - 20));
 
-		stage.widthProperty().addListener((ChangeListener<Number>) (arg0, oldValue, newValue) -> contentBox.setWrappingWidth(newValue.doubleValue() - 20));
-		
 		lowerPane.getChildren().add(contentBox);
 
-		vbox.getChildren().addAll(menuBar, splitPane);
+		vbox.getChildren().addAll(toolBar, splitPane);
 
 		vbox.prefHeightProperty().bind(stage.heightProperty());
 
 		stage.setScene(scene);
 		stage.show();
 	}
-
+	
 	private void showErrorDialog() {
 		Stage dialogStage = new Stage();
-		dialogStage.initModality(javafx.stage.Modality.WINDOW_MODAL);
-		dialogStage.setScene(new Scene(VBoxBuilder.create()
-				.children(new Text("Error"), new Button("Ok."))
-				.alignment(javafx.geometry.Pos.CENTER).padding(new Insets(5))
-				.build()));
+		dialogStage.initModality(Modality.WINDOW_MODAL);
+		VBox vBox = new VBox();
+		vBox.getChildren().addAll(new Text("Error"), new Button("Ok."));
+		vBox.setAlignment(Pos.CENTER);
+		vBox.setPadding(new Insets(5));
+		
+		dialogStage.setScene(new Scene(vBox));
 		dialogStage.show();
 	}
 
@@ -190,49 +226,17 @@ public class Starter extends Application {
 						"toLocation"));
 
 		ObservableList<TableColumn<Clipping, ?>> columns = table.getColumns();
-		columns.addAll(title, content, type, fromLocation, toLocation);
+		columns.add(title);
+		columns.add(content);
+		columns.add(type);
+		columns.add(fromLocation);
+		columns.add(toLocation);
 
 		table.prefWidthProperty().bind(readOnlyDoubleProperty);
 
 		return table;
 	}
 
-	private MenuBar buildMenuBarWithMenus(
-			final ReadOnlyDoubleProperty menuWidthProperty) {
-		final MenuBar menuBar = new MenuBar();
-
-		// Prepare left-most "File" drop-down menu
-
-		// Prepare "Examples" drop-down menu
-
-		// Prepare "Help" drop-down menu
-		final Menu fileMenu = new Menu("File");
-		final MenuItem fileOpenItem = MenuItemBuilder
-				.create()
-				.text("Open File")
-				.onAction(new OpenHandler())
-				.accelerator(
-						new KeyCodeCombination(KeyCode.O,
-								KeyCombination.CONTROL_DOWN)).build();
-		fileMenu.getItems().add(fileOpenItem);
-		fileMenu.getItems().add(new SeparatorMenuItem());
-		final MenuItem exitItem = MenuItemBuilder
-				.create()
-				.text("Exit")
-				.onAction(actionEvent -> System.exit(0))
-				.accelerator(
-						new KeyCodeCombination(KeyCode.X,
-								KeyCombination.CONTROL_DOWN)).build();
-
-		fileMenu.getItems().add(exitItem);
-
-		menuBar.getMenus().add(fileMenu);
-
-		// bind width of menu bar to width of associated stage
-		menuBar.prefWidthProperty().bind(menuWidthProperty);
-
-		return menuBar;
-	}
 
 	private static void fillTable(TableView<Clipping> table2,
 			List<Clipping> clippings) {
